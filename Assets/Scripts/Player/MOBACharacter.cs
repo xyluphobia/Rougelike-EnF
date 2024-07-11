4369a7bccc2f1d9ce78f950ca1c999c995f3afd9
Matthew <84782280+xyluphobia@users.noreply.github.com>
2024-07-11 12:04:07 -0400

    Basic movement for MOBA character/sync

--- Assets/Scripts/Player/MOBACharacter.cs
+++ Assets/Scripts/Player/MOBACharacter.cs
@@ -4,6 +4,27 @@ using UnityEngine;
 
 public class MOBACharacter : MonoBehaviour
 {
+    private PlayerController playerController;
+
+    Vector2 lastClickedPos;
+
+    private void Start()
+    {
+        playerController = GetComponent<PlayerController>();
+    }
+
+    private void FixedUpdate()
+    {
+        if ((Vector2)transform.position != lastClickedPos)
+        {
+            float step = playerController.movementSpeed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, lastClickedPos, step);
+        }
+        else
+        {
+            playerController.animator.SetFloat("Speed", 0);
+        }
+    }
 
     public void TakeControl()
     {
@@ -12,6 +33,25 @@ public class MOBACharacter : MonoBehaviour
 
     private void OnMove_MOBA()
     {
+        lastClickedPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
+        float diffX = lastClickedPos.x - transform.position.x;
+        float diffY = lastClickedPos.y - transform.position.y;
+
+        playerController.animator.SetFloat("Horizontal", diffX);
+        playerController.animator.SetFloat("Vertical", diffY);
+        playerController.animator.SetFloat("Speed", lastClickedPos.sqrMagnitude);
+
+
+        if (diffX > diffY)
+        {
+            diffY = (diffY / diffX) * 100;
+            diffX = 1;
+            playerController.animator.SetFloat("lastHorizontal", diffX);
+        }
+        else
+        {
+            playerController.animator.SetFloat("lastVertical", diffY);
+        }
     }
 }
