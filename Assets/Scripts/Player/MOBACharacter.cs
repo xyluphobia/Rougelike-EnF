c71a96a9b50ba4565ec374e7f001e859d33984af
Matthew <mattscavone11@gmail.com>
2024-07-22 05:52:59 -0400

    Added NavMeshPlus plugin to allow for better pathing of both the MOBA character and enemy AI. Currently implementing the navmesh on the MOBA character and have not started on AI. Appears to work well on premade levels currently not working on procedural generated levels.

--- Assets/Scripts/Player/MOBACharacter.cs
+++ Assets/Scripts/Player/MOBACharacter.cs
@@ -1,21 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class MOBACharacter : MonoBehaviour
 {
     private PlayerController playerController;
+    private NavMeshAgent navAgent;
 
     Vector2 lastClickedPos;
 
     private void Start()
     {
         playerController = GetComponent<PlayerController>();
+
+        navAgent = GetComponentInChildren<NavMeshAgent>();
+        navAgent.updateRotation = false;
+        navAgent.updateUpAxis = false;
+
         lastClickedPos = transform.position;
     }
 
     private void FixedUpdate()
-    {
+    {/*
         if ((Vector2)transform.position != lastClickedPos)
         {
             float step = playerController.movementSpeed * Time.deltaTime;
@@ -24,7 +31,7 @@ public class MOBACharacter : MonoBehaviour
         else
         {
             playerController.animator.SetFloat("Speed", 0);
-        }
+        }*/
     }
 
     public void TakeControl()
@@ -35,7 +42,7 @@ public class MOBACharacter : MonoBehaviour
     private void OnMove_MOBA()
     {
         lastClickedPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
+        /*
         float diffX = lastClickedPos.x - transform.position.x;
         float diffY = lastClickedPos.y - transform.position.y;
 
@@ -54,5 +61,7 @@ public class MOBACharacter : MonoBehaviour
         {
             playerController.animator.SetFloat("lastVertical", diffY);
         }
+        */
+        navAgent.SetDestination(new Vector3(lastClickedPos.x, lastClickedPos.y));
     }
 }
--- Assets/Scripts/ProceduralLevelGeneration/GenerationAlgorithms/CorridorFirstGenerator.cs
+++ Assets/Scripts/ProceduralLevelGeneration/GenerationAlgorithms/CorridorFirstGenerator.cs
@@ -1,3 +1,4 @@
+using NavMeshPlus.Components;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -12,8 +13,13 @@ public class CorridorFirstGenerator : SimpleRandomWalkGenerator
     [Range(0.1f, 1f)]
     private float roomPercent = 0.8f;
 
+    private NavMeshSurface Surface2D;
     public override void RunProceduralGeneration() {
         CorridorFirstGeneration();
+
+        // Bake in Nav Mesh at run time.
+        Surface2D = GameObject.FindGameObjectWithTag("TilemapVisualizer").GetComponent<NavMeshSurface>();
+        Surface2D.BuildNavMeshAsync();
     }
 
     private void CorridorFirstGeneration() {
