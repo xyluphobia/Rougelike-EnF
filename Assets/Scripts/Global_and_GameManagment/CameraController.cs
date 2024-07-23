0692ee537d33e1c3c832643739b59f30b9ea0fb2
Matthew <mattscavone11@gmail.com>
2024-07-23 05:30:36 -0400

    Camera now moves smoothly when swapping characters and moving. Additionally the camera now slightly leads the player based on the direction they are moving providing more useful vision.

--- Assets/Scripts/Global_and_GameManagment/CameraController.cs
+++ Assets/Scripts/Global_and_GameManagment/CameraController.cs
@@ -4,23 +4,55 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public Transform target;
+    public GameObject target;
+    private Vector3 targetPoint;
 
+    private readonly float lookAheadSpeed = 5f;
+    private readonly float lookAheadDistanceX = 2.5f;
+    private readonly float lookAheadDistanceY = 1.5f;
+    private Vector2 lookOffset;
+
+    private Vector2 targetCurrentPosition;
+    private Vector2 targetLastPosition;
 
     void Start()
     {
         SetCameraTarget();
+        targetPoint.z = transform.position.z;
+        targetLastPosition = target.transform.position;
     }
     public void SetCameraTarget(GameObject targetToSet = null)
     {
         if (targetToSet != null)
-            target = targetToSet.transform;
+            target = targetToSet;
         else
-            target = GameObject.FindGameObjectWithTag("Player").transform;
+            target = GameObject.FindGameObjectWithTag("Player");
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+        //transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+        targetCurrentPosition = target.transform.position;
+
+        if (targetCurrentPosition.x > targetLastPosition.x)
+            lookOffset.x = Mathf.Lerp(lookOffset.x, lookAheadDistanceX, lookAheadSpeed * Time.deltaTime);
+        else if (targetCurrentPosition.x < targetLastPosition.x)
+            lookOffset.x = Mathf.Lerp(lookOffset.x, -lookAheadDistanceX, lookAheadSpeed * Time.deltaTime);
+        else
+            lookOffset.x = Mathf.Lerp(lookOffset.x, 0, 4f * Time.deltaTime);
+
+        if (targetCurrentPosition.y > targetLastPosition.y)
+            lookOffset.y = Mathf.Lerp(lookOffset.y, lookAheadDistanceY, lookAheadSpeed * Time.deltaTime);
+        else if (targetCurrentPosition.y < targetLastPosition.y)
+            lookOffset.y = Mathf.Lerp(lookOffset.y, -lookAheadDistanceY, lookAheadSpeed * Time.deltaTime);
+        else
+            lookOffset.y = Mathf.Lerp(lookOffset.y, 0, 4f * Time.deltaTime);
+
+        targetPoint.x = targetCurrentPosition.x + lookOffset.x;
+        targetPoint.y = targetCurrentPosition.y + lookOffset.y;
+
+
+        transform.position = Vector3.Lerp(transform.position, targetPoint, 4f * Time.deltaTime);
+        targetLastPosition = target.transform.position;
     }
 }
